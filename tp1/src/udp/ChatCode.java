e4920a99a8d30a32e36202741f38a2d17c9410cf
Alder Hugo <alder@a10p17.fil.univ-lille1.fr>
2017-01-12 15:15:24 +0100

    Encodage/Decodage + .gitignore

--- tp1/src/udp/ChatCode.java
+++ tp1/src/udp/ChatCode.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class ChatCode {
@@ -12,13 +16,16 @@ public class ChatCode {
 	public static void main(String[] args) throws IOException {
 
 		int port = 7654;
-		InetAddress groupAddress = InetAddress.getByName("224.0.0.2");
+		String address = "224.0.0.2";
+		InetAddress groupAddress = InetAddress.getByName(address);
 		MulticastSocket socket = new MulticastSocket(port);
 		socket.joinGroup(groupAddress);
 
 		Thread sender = new Thread() {
 			public void run() {
 
+				System.out.println("Server started");
+				
 				String message = "";
 				Scanner scan = new Scanner(System.in);
 				while (true) {
@@ -26,7 +33,7 @@ public class ChatCode {
 					message += scan.nextLine();
 
 					// Encodage
-					message = code(message, 5);
+					message = code(message, getKey());
 
 					data = message.getBytes();
 
@@ -53,10 +60,10 @@ public class ChatCode {
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					
+
 					// Decodage
-					String tmp = decode(new String(data, 0, packet.getLength()), 5);
-					
+					String tmp = decode(new String(data, 0, packet.getLength()), getKey());
+
 					String message = packet.getAddress().getHostName() + " " + tmp;
 					System.out.println(message);
 					data = new byte[1024];
@@ -87,6 +94,33 @@ public class ChatCode {
 			res += c;
 		}
 		return res;
+	}
+	
+	public static String getMacAddress() {
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			Enumeration<NetworkInterface> network = NetworkInterface.getNetworkInterfaces();
+			while (network.hasMoreElements()) {
+				NetworkInterface ni = network.nextElement();
+				byte[] mac = ni.getHardwareAddress();
+				if (mac != null) {
+					String res = "";
+					for (int i = 0; i < mac.length; i++) {
+						res += String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : "");
+					}
+					return res;
+				}
+			}
+		} catch (UnknownHostException | SocketException e1) {
+			e1.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static int getKey() {
+		String macAddress = getMacAddress();
+		String[] tab = macAddress.split("-");
+		return Integer.parseInt(tab[tab.length - 1], 16);
 	}
 
 }
